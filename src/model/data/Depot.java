7f8e49ec7e7a83611713f34f3d4a525ec61727c6
Mihai <mihai.mihalache@stud.ubbcluj.ro>
2022-11-05 21:09:48 +0200

    completed tests added javadoc

--- src/model/data/Depot.java
+++ src/model/data/Depot.java
@@ -22,44 +22,38 @@ public class Depot {
         this.vehicles = new ArrayList<>();
     }
 
-
+    /**
+     * Gets all Vehicles from the depot
+     * @return list of Vehicles
+     */
     public List<Vehicle> getVehicles() {
         return vehicles;
     }
 
+    /**
+     * sets the given List as the depot's vehicles
+     * @param vehicles list of vehicles to be set
+     */
     public void setVehicles(List<Vehicle> vehicles) {
         this.vehicles = vehicles;
     }
 
+    /**
+     * adds a given vehicle to a depot
+     * @param v vehicle to be added to a depot
+     */
     public void addVehicle(Vehicle v)
     {
-
         vehicles.add(v);
     }
 
+    /**
+     * removes a given Vehicle from a Depot
+     * @param v Vehicle to be removed from a depot
+     */
     public void removeVehicle(Vehicle v)
     {
-
         vehicles.remove(v);
-    }
-
-
-
-    public void showVehicle()
-    {
-
-        for(int i=0;i<vehicles.size();i++)
-        {
-            System.out.println(vehicles.get(i));
-        }
-
-    }
-
-    @Override
-    public String toString() {
-        return "Depos{" +
-                "name='" + name + '\'' +
-                '}';
     }
 
 }
